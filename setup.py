9e1ad56ae786611b5c1caed547e6b12f9f45c616
Mike Shoup <mike@shoup.io>
2019-01-03 12:54:08 -0700

    Bump version: 0.0.4 → 0.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requires = [
 
 setup(
     name='synthale',
-    version='0.0.4',
+    version='0.1.0',
     url='https://github.com/shouptech/synthale',
     author='Mike Shoup',
     author_email='mike@shoup.io',
